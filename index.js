3a9857219f76df3f2e5eb7ce070d357d618373b3
SamedovRasul <samedovrasul7@gmail.com>
2023-03-03 18:52:12 +0400

    .env

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ app.post('/post', (req, res) =>{
 
 })
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 
 
 mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
